d8cc2db176a9117276ffdb0f79a280a153dfb858
Ardi Jorganxhi <ardijorganxhi@eskisehir.edu.tr>
2023-02-01 21:14:38 +0100

    Initial Commit

---
+++ ObjectDetector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ObjectDetector
+//
+//  Created by Ardi Jorganxhi on 1.2.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
